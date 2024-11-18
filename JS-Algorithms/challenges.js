d29383a71998c7caf9f427224f08d91602d28f0a
Steven Lara <stevencarcamo@ravn.co>
2024-11-18 14:18:58 -0600

    refactor(readableTime): Refactor code

--- JS-Algorithms/challenges.js
+++ JS-Algorithms/challenges.js
@@ -13,23 +13,11 @@ Invoking "readableTime(3690)" should return "01:01:30" (HH:MM:SS)
 ***** */
 
 const readableTime = (seconds) => {
-  const minutesFromSeconds = seconds / 60;
-  const hoursFromMinutes = Math.floor(minutesFromSeconds / 60);
-  const leftMinutes = Math.floor(minutesFromSeconds - (hoursFromMinutes * 60));
-  const leftSeconds = seconds - (hoursFromMinutes * 60 * 60) - (leftMinutes * 60);
+  const hours = Math.floor(seconds / 3600).toString().padStart(2, '0');
+  const minutes = Math.floor((seconds % 3600) / 60).toString().padStart(2, '0');
+  const secondsLeft = (seconds % 60).toString().padStart(2, '0');
 
-  const time = {
-    hours: hoursFromMinutes,
-    minutes: leftMinutes,
-    seconds: leftSeconds
-  };
-
-  Object.keys(time).forEach((value, index, array) => {
-    const formattedValue = String(time[value]).padStart(2, 0);
-    time[value] = formattedValue;
-  });
-
-  return `${time.hours}:${time.minutes}:${time.seconds}`;
+  return `${hours}:${minutes}:${secondsLeft}`;
 };
 
 readableTime(458);
